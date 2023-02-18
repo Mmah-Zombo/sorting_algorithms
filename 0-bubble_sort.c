762e40b1c5b91e0b34b1eb2eb8552c8b9f8de47b
M'mah Zombo <www.mariamzombo@gmail.com>
2023-02-18 06:01:40 +0000

    fixed the correct output error

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -13,8 +13,11 @@ void bubble_sort(int *array, size_t size)
     size_t temp;
 
     for (; i < size; i++) {
-        for (j = 0; j < size; j++) {
+
+        for (j = 0; j < size - 1; j++) {
+
             if (array[j] > array[j + 1]) {
+                
                 temp = array[j];
                 array[j] = array[j + 1];
                 array[j + 1] = temp;
